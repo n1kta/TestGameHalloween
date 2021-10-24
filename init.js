4049057692f2fe33b4926eaa1f7114d54acccc8f
n1kta <niga200213@gmail.com>
2021-10-24 22:15:35 +0300

    first commit

---
+++ init.js
@@ -0,0 +1,26 @@
+const config = {
+    type: Phaser.AUTO,
+    width: 1280,
+    height: 720,
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+const game = new Phaser.Game(config);
+
+function preload ()
+{
+    
+}
+
+function create ()
+{
+    
+}
+
+function update ()
+{
+}
