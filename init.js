d19aef1ba7c0692ec37a6b3c0d7c2d08d4b0ff1a
fantasseryoga <69531863+fantasseryoga@users.noreply.github.com>
2021-10-24 14:45:41 -0700

    test dude
    test dude

--- init.js
+++ init.js
@@ -6,21 +6,70 @@ const config = {
         preload: preload,
         create: create,
         update: update
-    }
+    },
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 0 },
+            debug: false
+        }
+    },
 };
 
 const game = new Phaser.Game(config);
 
 function preload ()
 {
-    
+    this.load.spritesheet('dude', 'assets/player/walking.png', { frameWidth: 100, frameHeight: 100 });
 }
 
 function create ()
 {
-    
+    player = this.physics.add.sprite(100, 100, 'dude');
+    // player.setBounce(0.2);
+
+    cursors = this.input.keyboard.createCursorKeys();
+
+    this.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('dude', { start: 7, end: 13 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    this.anims.create({
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 6 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    this.anims.create({
+        key: 'turn',
+        frames: [ {key: 'dude', frame: 0} ],
+        frameRate: 10,
+    });
 }
 
 function update ()
 {
+
+    if (cursors.left.isDown)
+    {
+        player.setVelocityX(-160);
+
+        player.anims.play('left', true);
+    }
+    else if (cursors.right.isDown)
+    {
+        player.setVelocityX(160);
+
+        player.anims.play('right', true);
+    }
+    else
+    {
+        player.setVelocityX(0);
+
+        player.anims.play('turn', true)
+    }
 }
